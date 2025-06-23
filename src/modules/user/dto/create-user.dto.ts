af5a833eb3c09d1521cd7ca739b98f85b6b202f2
Dev Aze <azevchk@gmail.com>
2025-06-23 14:25:27 -0300

    jwt files / feat

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsString, IsNotEmpty, MinLength, Validate, } from 'class-validator';
+import { Match } from 'src/utils/match.decorator';
+
+export class CreateUserDto{
+
+    @IsString()
+    @IsNotEmpty()
+    nome!: string;
+
+    @IsString()
+    @MinLength(5)
+    senha!: string;
+
+    @IsString()
+    @Validate(Match, ['senha'], {
+        message: 'As senhas não batem :('
+    })
+    confirmarSenha!: string;
+
+    @IsString()
+    email!: string;
+
+
+    @IsString()
+    usuario!: string;
+
+   
+}
