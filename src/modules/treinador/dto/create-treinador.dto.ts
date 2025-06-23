37d3051afee59413dde9250ee6da16e23034fd3e
KaueFon <kauefontes@gmail.com>
2025-06-23 18:11:10 -0300

    adicionado modules, controller, service e create dto de exercicio, treino e treinador

--- src/modules/treinador/dto/create-treinador.dto.ts
+++ src/modules/treinador/dto/create-treinador.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTreinadorDto {}
+import { IsString, IsNotEmpty, IsOptional, IsEmail, IsPhoneNumber } from 'class-validator';
+
+export class CreateTreinadorDto {
+  @IsString()
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  nome!: string;
+
+  @IsString()
+  @IsOptional()
+  especialidade?: string;
+
+  @IsEmail({}, { message: 'Email inválido' })
+  email!: string;
+
+  @IsString()
+  @IsOptional()
+  @IsPhoneNumber(null, { message: 'Telefone inválido' })
+  telefone?: string;
+}
